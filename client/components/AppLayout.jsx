d716f09923dbe98e6c14f205c5def86d32f58528
happydhKim <happytime870616@gmail.com>
2019-10-28 18:24:09 +0900

    [ FEATURE ] 회원가입 폼 ui 그리기 및 state 관리하기 (#7, #8)
    Why?
    - 회원가입을 위한 동작이 필요하다.
    - 상태변화에 따른 대응이 필요하다.
    
    What?
    - 훅을 이용해 state 를 관리한다.
    - 간단하게 submit이 잘 나오는지 확인한다 (console.log)

--- client/components/AppLayout.jsx
+++ client/components/AppLayout.jsx
@@ -6,12 +6,12 @@ const AppLayout = ({ children }) => (
   <div>
     <Menu mode="horizontal">
       <Menu.Item key="logo">로고</Menu.Item>
-      <Menu.Item key="">
+      <Menu.Item key="search">
         <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
       </Menu.Item>
-      <Menu.Item key="">더미</Menu.Item>
-      <Menu.Item key=""><Link href="/Signup"><a>회원가입</a></Link></Menu.Item>
-      <Menu.Item key=""><Link href="/Profile"><a>회원정보</a></Link></Menu.Item>
+      <Menu.Item key="nothing">더미</Menu.Item>
+      <Menu.Item key="signup"><Link href="/Signup"><a>회원가입</a></Link></Menu.Item>
+      <Menu.Item key="profile"><Link href="/Profile"><a>회원정보</a></Link></Menu.Item>
     </Menu>
     {children}
   </div>
--- client/pages/Signup.jsx
+++ client/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Form, Input, Checkbox, Button
 } from 'antd';
@@ -6,12 +6,45 @@ import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
 
 const Signup = () => {
-  const onSubmit = () => { };
-  const onChangeId = () => { };
-  const onChangeNickName = () => { };
-  const onChangePassword = () => { };
-  const onChangePasswordChk = () => { };
-  const onChangeAgree = () => { };
+  const commonInput = (initValue = null) => {
+    const [value, setter] = useState(initValue);
+    const handler = (e) => {
+      setter(e.target.value);
+    };
+    return [value, handler];
+  };
+
+  const [id, onChangeId] = commonInput('');
+  const [nickname, onChangeNickName] = commonInput('');
+  const [password, onChangePassword] = commonInput('');
+
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [agree, setAgree] = useState(false);
+
+  const [passwordError, setPasswordError] = useState(false);
+  const [agreeError, setAgreeError] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+    if (!agree) {
+      return setAgreeError(true);
+    }
+    console.log({
+      id, nickname, password, passwordCheck, agree
+    });
+  };
+  const onChangePasswordCheck = (e) => {
+    setPasswordError(e.target.value !== password);
+    setPasswordCheck(e.target.value);
+  };
+  const onChangeAgree = (e) => {
+    setAgreeError(false);
+    setAgree(e.target.checked);
+  };
+
   return (
     <>
       <Head>
@@ -20,29 +53,31 @@ const Signup = () => {
       </Head>
       <AppLayout>
         <div>회원가입</div>
-        <Form>
+        <Form onSubmit={onSubmit} style={{ paddind: 10, margin: 10 }}>
           <div>
             <label htmlFor="user-id">아이디</label>
             <br />
-            <Input name="user-id" required onChange={onChangeId} />
+            <Input name="user-id" value={id} required onChange={onChangeId} />
           </div>
           <div>
             <label htmlFor="user-nick">닉네임</label>
             <br />
-            <Input name="user-nick" required onChange={onChangeNickName} />
+            <Input name="user-nick" value={nickname} required onChange={onChangeNickName} />
           </div>
           <div>
             <label htmlFor="user-password">비밀번호</label>
             <br />
-            <Input name="user-password" type="password" required onChange={onChangePassword} />
+            <Input name="user-password" type="password" value={password} required onChange={onChangePassword} />
           </div>
           <div>
             <label htmlFor="user-password-check">비밀번호 확인</label>
             <br />
-            <Input name="user-password-check" type="password" required onChange={onChangePasswordChk} />
+            <Input name="user-password-check" type="password" value={passwordCheck} required onChange={onChangePasswordCheck} />
+            {passwordError && <div style={{ color: 'red' }}>비밀번호를 다시 확인해 주세요.</div>}
           </div>
           <div>
-            <Checkbox name="user-agree" value="" onChange={onChangeAgree}>약관에 동의합니다.</Checkbox>
+            <Checkbox name="user-agree" checked={agree} onChange={onChangeAgree}>약관에 동의합니다.</Checkbox>
+            {agreeError && <div style={{ color: 'red' }}>약관에 동의해주세요.</div>}
           </div>
           <div>
             <Button type="primary" htmlType="submit">가입하기</Button>
