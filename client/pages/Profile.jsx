4644039a0a92d8db7ab59a9d9dfe9a7c39a93aca
happydhKim <happytime870616@gmail.com>
2019-10-28 22:47:24 +0900

    [ FEATURE ] 기본 ui 그리기 : 프로필 (#7)
    Why?
    - 내 프로필 및 팔로잉 팔로워를 볼 수 있는 공간이 필요하다.
    
    What?
    - 기본적인 프로필 컴퍼넌트 ui 를 그려 놓는다.

--- client/pages/Profile.jsx
+++ client/pages/Profile.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
+import {
+  Form, Input, Button, List, Icon, Card
+} from 'antd';
 
 const Profile = () => (
   <>
-    <div>회원정보</div>
+    <div>
+      <Form style={{ marginBottom: 20, border: '1px solid #e9e9e9', padding: 20 }}>
+        <Input addonBefore="닉네임" />
+        <Button type="primary">수정</Button>
+        <List
+          style={{ marginBottom: 20 }}
+          grid={{ gutter: 1, xs: 1, md: 1 }}
+          size="small"
+          header={<div>팔로잉</div>}
+          loadMore={<Button style={{ width: 100 }}>더 보기</Button>}
+          bordered
+          dataSource={['1', '2', '3']}
+          renderItem={(item) => (
+            <List.Item style={{ marginTop: 20 }}>
+              <Card actions={[<Icon type="stop" />]}><Card.Meta description={item} /></Card>
+            </List.Item>
+          )}
+        />
+        <List
+          style={{ marginBottom: 20 }}
+          grid={{ gutter: 1, xs: 1, md: 1 }}
+          size="small"
+          header={<div>팔로워</div>}
+          loadMore={<Button style={{ width: 100 }}>더 보기</Button>}
+          bordered
+          dataSource={['1', '2', '3']}
+          renderItem={(item) => (
+            <List.Item style={{ marginTop: 20 }}>
+              <Card actions={[<Icon type="stop" />]}><Card.Meta description={item} /></Card>
+            </List.Item>
+          )}
+        />
+      </Form>
+    </div>
   </>
 );
 
