c6542e5f14c9353d30e57df30f3bda8ccdcfd260
happydhKim <happytime870616@gmail.com>
2019-10-28 22:20:32 +0900

    [ FEATURE ] 기본 ui 그리기 (#7)
    Why?
    - 기본적인 댓글을 그려야 작업을 진행할 수 있다.
    
    What?
    - 미리 작업을 해두기 위해 card 형태로 밑그림을 그림
    - 추 후에 컴퍼넌트 분리 및 알맞은 형태로 ui 를 가꿀 예정

--- client/components/AppLayout.jsx
+++ client/components/AppLayout.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import {
   Menu, Input, Modal, Button
 } from 'antd';
-import Login from "./Login";
+import Login from './Login';
 
 const AppLayout = ({ children }) => {
   const [state, setState] = useState(false);
@@ -36,8 +36,8 @@ const AppLayout = ({ children }) => {
           <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
         </Menu.Item>
         <Menu.Item key="nothing">더미</Menu.Item>
-        <Menu.Item key="signup"><Link href="/Signup"><a>회원가입</a></Link></Menu.Item>
-        <Menu.Item key="profile"><Link href="/Profile"><a>회원정보</a></Link></Menu.Item>
+        <Menu.Item key="signup"><Link href="/signup"><a>회원가입</a></Link></Menu.Item>
+        <Menu.Item key="profile"><Link href="/profile"><a>회원정보</a></Link></Menu.Item>
         <Menu.Item key="login"><Button type="primary" onClick={showModal}>로그인</Button></Menu.Item>
         <Modal
           title="로그인을 해주세요"
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import {
+  Form, Input, Button, Card, Avatar, Icon
+} from 'antd';
 
-const Main = () => (
-  <>
-    <div>메인 페이지 입니다.</div>
-  </>
-);
+const dummy = {
+  isLoggedIn: true,
+  imagePaths: [],
+  mainPosts: [{
+    User: {
+      id: 1,
+      nickname: ' kim',
+      content: '1댓글'
+    }
+  }]
+};
 
+const Main = () => {
+  const a = 3;
+  return (
+    <>
+      <div>
+        {dummy.isLoggedIn && (
+          <Form encType="multipart/form-data">
+            <Input.TextArea maxLength={300} placeholder="욕설은 나빠요! 예쁜말만 해주세요." />
+            <div>
+              <input type="file" multiple hidden />
+              <Button>이미지 업로드</Button>
+              <Button type="primary" style={{ float: 'right' }} htmlType="submit">이미지올리기</Button>
+            </div>
+            <div>
+              {dummy.imagePaths.map((v, i) => (
+                <div key={v} style={{ display: 'inline-block' }}>
+                  <img src={`http://localhost:3005/${v}`} stype={{ width: 200 }} alt={v} />
+                  <div><Button>x</Button></div>
+                </div>
+              ))}
+            </div>
+          </Form>
+        )}
+        {dummy.mainPosts.map((c) => (
+          <Card
+            key={+c.createdAt}
+            cover={c.img && <img alt="test" src={c.img} />}
+            actions={[
+              <Icon type="retweet" key="retweet" />,
+              <Icon type="heart" key="heart" />,
+              <Icon type="like" key="like" />,
+              <Icon type="dislike" key="dislike" />
+            ]}
+          >
+            <Card.Meta
+              avatar={<Avatar>{c.User.nickname[0]}</Avatar>}
+              title={c.User.nickname}
+              description={c.content}
+            />
+          </Card>
+        ))}
+      </div>
+    </>
+  );
+};
 export default Main;
