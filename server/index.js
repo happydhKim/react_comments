4ad4c4e66dde7aebe36bc557956d2fc3ea52dafc
happydhKim <happytime870616@gmail.com>
2019-10-31 17:35:08 +0900

    [ FEATURE ] API 라우터로 분리 시키기 (#10)
    Why?
    - 익스프레스에서 많은 API들을 한 곳에 집중 시키면 코드가 복잡해 진다.
    - 분리해서 관리할 필요가 있다.
    
    What?
    - 크게 분리를 해서 routes 폴더에 나눠 넣는다.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const db = require('./models');
 
 const app = express();
+db.sequelize.sync();
+const userAPIRouter = require('./routes/user');
+const commentAPIRouter = require('./routes/comment');
+const commentsAPIRouter = require('./routes/comments');
+const hashtagAPIRouter = require('./routes/hashtag');
 
 app.get('/', (req, res) => {
   res.send('익스프레스 안녕~');
 });
+
+app.use('/api/user', userAPIRouter);
+app.use('/api/post', commentAPIRouter);
+app.use('/api/posts', commentsAPIRouter);
+app.use('/api/hashtag', hashtagAPIRouter);
 
 app.listen(3005, () => {
   console.log('서버가 작동중이에요! http://localhost:3005');
